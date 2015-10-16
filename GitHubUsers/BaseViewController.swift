72d4017dd593b3c2ac2a0abfa4718a10161e392d
PPoborca <patryk.poborca@isobar.com>
2015-10-15 23:35:27 -0500

    uploading

---
+++ GitHubUsers/BaseViewController.swift
@@ -0,0 +1,15 @@
+//
+//  BaseViewController.swift
+//  GitHubUsers
+//
+//  Created by PPoborca on 10/15/15.
+//  Copyright (c) 2015 PPoborca. All rights reserved.
+//
+
+import Foundation
+
+class BaseViewController  {
+    
+
+    
+}
--- GitHubUsers/ViewController.swift
+++ GitHubUsers/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        makeGetRequest();
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func makeGetRequest(){
+        var url : String = "https://api.github.com/users"
+        var request : NSMutableURLRequest = NSMutableURLRequest()
+        request.URL = NSURL(string: url)
+        request.HTTPMethod = "GET"
+        request.timeoutInterval = 120
+
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue(), completionHandler:{ (response:NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+
+            var error: NSError?
+            if data != nil {
+            var test  =  NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &error) as NSDictionary?
+            }
+            var asdfs = 123;
+        })
+    }
 
 }
 
